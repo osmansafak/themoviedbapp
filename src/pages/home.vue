c0245251619ff92ed90a18d1ab4aecc948eca887
Osman Safak <osmansafak@gmail.com>
2020-03-06 08:37:42 +0100

    change image size

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,42 +1,41 @@
 <script>
-    import SwiperContent from './swiper.vue';
-    import Loader from './loader.vue';
+import SwiperContent from "./swiper.vue";
+import Loader from "./loader.vue";
 
-    export default {
-        name: 'home',
-        data() {
-            return {}
-        },
-        components: {
-            SwiperContent,
-            Loader,
-        },
-        computed: {
-            allMovies () {
-                return this.$store.state.carousel;
-            },
-        },
-        created () {
-            this.$store.dispatch('GET_CAROUSEL');
-        },
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  components: {
+    SwiperContent,
+    Loader
+  },
+  computed: {
+    allMovies() {
+      return this.$store.state.carousel;
     }
+  },
+  created() {
+    this.$store.dispatch("GET_CAROUSEL");
+  }
+};
 </script>
 <template>
-    <f7-page class="bg-white" :key="'/'">
-
-        <!-- Page Content -->
-        <loader v-if="!allMovies.length"/>
-        <f7-swiper pagination :params="{loop:true}" v-if="allMovies.length">
-            <f7-swiper-slide v-for="movie in allMovies" :key="movie.id">
-                <f7-link :href="'/movie/'+movie.id">
-                    <img :src="'https://image.tmdb.org/t/p/w500'+movie.backdrop_path" width="100%">
-                    <span>{{movie.title}}</span>
-                </f7-link>
-            </f7-swiper-slide>
-        </f7-swiper>
-        <swiper-content :datatype="'now_playing'" :datatitle="'Now Playing'"/>
-        <swiper-content :datatype="'popular'" :datatitle="'Popular'"/>
-        <swiper-content :datatype="'top_rated'" :datatitle="'Top Rated'"/>
-        <swiper-content :datatype="'upcoming'" :datatitle="'Upcoming'"/>
-    </f7-page>
+  <f7-page class="bg-white" :key="'/'">
+    <!-- Page Content -->
+    <loader v-if="!allMovies.length" />
+    <f7-swiper pagination :params="{loop:true}" v-if="allMovies.length">
+      <f7-swiper-slide v-for="movie in allMovies" :key="movie.id">
+        <f7-link :href="'/movie/'+movie.id">
+          <img :src="'https://image.tmdb.org/t/p/original'+movie.backdrop_path" width="100%" />
+          <span>{{movie.title}}</span>
+        </f7-link>
+      </f7-swiper-slide>
+    </f7-swiper>
+    <swiper-content :datatype="'now_playing'" :datatitle="'Now Playing'" />
+    <swiper-content :datatype="'popular'" :datatitle="'Popular'" />
+    <swiper-content :datatype="'top_rated'" :datatitle="'Top Rated'" />
+    <swiper-content :datatype="'upcoming'" :datatitle="'Upcoming'" />
+  </f7-page>
 </template>
--- src/pages/swiper.vue
+++ src/pages/swiper.vue
@@ -1,40 +1,47 @@
 <script>
-    import Loader from './loader.vue';
-    export default {
-        name: 'swiper',
-        props: ['datatype', 'datatitle'],
-        data() {
-            return {}
-        },
-        components: {
-            Loader,
-        },
-        computed: {
-            allMovies () {
-                let state_name=this.datatype;
-                return this.$store.state[state_name];
-
-            },
-        },
-        created () {
-            this.$store.dispatch('GET_SWIPER', {
-                type: this.datatype,
-            });
-        },
+import Loader from "./loader.vue";
+export default {
+  name: "swiper",
+  props: ["datatype", "datatitle"],
+  data() {
+    return {};
+  },
+  components: {
+    Loader
+  },
+  computed: {
+    allMovies() {
+      let state_name = this.datatype;
+      return this.$store.state[state_name];
     }
+  },
+  created() {
+    this.$store.dispatch("GET_SWIPER", {
+      type: this.datatype
+    });
+  }
+};
 </script>
 <template>
-    <section>
-        <f7-block-title>
-            {{ datatitle }}
-            <f7-link color="red" :href="'/movies/' + datatype + '/futured/' + datatitle" text="See All"
-                     style="float: right;"></f7-link>
-        </f7-block-title>
-        <loader v-if="!allMovies.length" />
-        <f7-block v-if="allMovies.length">
-            <f7-swiper :params="{slidesPerView:3}">
-                <f7-swiper-slide v-for="movie in allMovies" :key="movie.id"><f7-link :href="'/movie/'+movie.id"><img :src="'https://image.tmdb.org/t/p/w150/' + movie.poster_path" width="75" ></f7-link></f7-swiper-slide>
-            </f7-swiper>
-        </f7-block>
-    </section>
+  <section>
+    <f7-block-title>
+      {{ datatitle }}
+      <f7-link
+        color="red"
+        :href="'/movies/' + datatype + '/futured/' + datatitle"
+        text="See All"
+        style="float: right;"
+      ></f7-link>
+    </f7-block-title>
+    <loader v-if="!allMovies.length" />
+    <f7-block v-if="allMovies.length">
+      <f7-swiper :params="{slidesPerView:3}">
+        <f7-swiper-slide v-for="movie in allMovies" :key="movie.id">
+          <f7-link :href="'/movie/'+movie.id">
+            <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" width="75" />
+          </f7-link>
+        </f7-swiper-slide>
+      </f7-swiper>
+    </f7-block>
+  </section>
 </template>
